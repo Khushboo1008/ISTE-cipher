68055c8dee9088ba436c6a01590c99edc98f39fc
Jai Malani <71269258+jai38@users.noreply.github.com>
2022-03-26 11:31:10 +0530

    level 1 updated

--- ceaserCipher.py
+++ ceaserCipher.py
@@ -1,5 +1,7 @@
 from random import randint
 import csv
+
+
 def get_range(number, size, min_value, max_value):
     all_ranges = []
     idx = 0
@@ -8,35 +10,35 @@ def get_range(number, size, min_value, max_value):
         c_max = number + idx
         if c_min >= min_value and c_max <= max_value:
             all_ranges.append([str(c_min), str(c_max)])
-        idx+=1
-    random_range_index = randint(0,len(all_ranges)-1)
+        idx += 1
+    random_range_index = randint(0, len(all_ranges)-1)
     return ' to '.join(all_ranges[random_range_index])
 
 
-def encrypt(text,s):
-	result = ""
-	for i in range(len(text)):
-		char = text[i]
-		if (char.isupper()):
-			result += chr((ord(char) + s-65) % 26 + 65)
-		else:
-			result += chr((ord(char) + s - 97) % 26 + 97)
-	return result
+def encrypt(text, s):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if (char.isupper()):
+            result += chr((ord(char) + s-65) % 26 + 65)
+        else:
+            result += chr((ord(char) + s - 97) % 26 + 97)
+    return result
+
+
 range_size = 3
-data = open('data.csv','r')
+data = open('data.csv', 'r')
 data = data.read().split()
-output = open('output.csv','w')
+output = open('output.csv', 'w')
 output = csv.writer(output)
 
-output.writerow(['Pc Name', 'Key', 'Range', 'Encrypted Message'])
+output.writerow(['Pc Name', 'Key', 'Range',
+                'Index(starting from 1)', 'Real Letter', 'Encrypted Message'])
 for pc_num in data:
-    key = randint(1,25)
+    key = randint(1, 25)
     current_range = get_range(key, range_size, 0, 25)
     en_msg = encrypt(pc_num, key)
-    output.writerow([pc_num, key, current_range, en_msg])
-
-text = "Techteam01"
-s = 5
-print ("Text : " + text)
-print ("Shift : " + str(s))
-print ("Cipher: " + encrypt(text,s))
+    real_char_index = randint(0, len(pc_num)-1)
+    real_char = pc_num[real_char_index]
+    output.writerow([pc_num, key, current_range,
+                    real_char_index+1, real_char, en_msg])
