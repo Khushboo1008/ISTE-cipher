6ca1344903d6726e1dd4a5c00a693f7ef841b5c3
Jai Malani <71269258+jai38@users.noreply.github.com>
2022-03-28 14:44:42 +0530

    dummy folders testing

--- ISTE/folder.py
+++ ISTE/folder.py
@@ -1,18 +1,33 @@
 import os
 import random
-path = 'C:\\Users\\khush\\Documents\\GitHub\\ISTE-cipher\\Dummy'
+from fpdf import FPDF
+import time
+
+path = 'C:\\Users\\Jai\\Documents\\GitHub\\ISTE-cipher\\ISTE\\Dummy'
 chars = 'qwertyuiopasdfghjklzxcvbnm'
 
-def get_rand_str (n):
+
+def get_rand_str(n):
     return ''.join(random.choice(chars) for _ in range(n))
-    
+
+
 def create_folder(n):
-    for _ in range(0,n):
+    for _ in range(0, n):
         os.mkdir(f'{path}//{get_rand_str(10)}')
 
     all_folders = os.listdir(path)
-    for folder in all_folders:
-        file = open(f'{path}\\{folder}\\{get_rand_str(10)}.txt','w')
-        file.write('Wrong File, Please try again')
+    for idx, folder in enumerate(all_folders):
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_font("Arial", size=15)
+        pdf.cell(200, 10, txt="Wrong File",
+                 ln=1, align='C')
+        pdf.cell(200, 10, txt="Please try again",
+                 ln=2, align='C')
+        pdf.output(f"{path}\\{folder}\\{get_rand_str(10)}.pdf")
+        print(folder, idx)
+        # file = open(f'{path}\\{folder}\\{get_rand_str(10)}.pdf', 'w')
+        # file.write('Wrong File, Please try again')
 
-create_folder(499)
+
+create_folder(10)
